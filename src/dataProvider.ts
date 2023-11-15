342ecfc9f70b1823916735f9d5696765214af1b4
Eduard Budacu <eduard.budacu@gmail.com>
2023-11-15 11:10:35 +0100

    extracted player and match logic in methods

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -10,35 +10,45 @@ export default class DataProvider {
           const [parsedData] = JSON.parse(data.toString());
 
           const result: PlayerData = {
-            players: [],
-            matches: []
+            players: this.loadPlayers(parsedData),
+            matches: this.loadMatches(parsedData)
           };
-          if (parsedData.players !== undefined && Array.isArray(parsedData.players)) {
-            result.players = parsedData.players.map((el: any): Player => {
-              return {
-                id: el.id,
-                name: el.name
-              };
-            });
-          }
 
-          if (parsedData.matches !== undefined && Array.isArray(parsedData.matches)) {
-            result.matches = parsedData.matches.map((el: any): Match => {
-              return {
-                playerId: el.playerId,
-                opponentId: el.opponentId,
-                result: el.result !== undefined && Array.isArray(el.result) ? el.result.map((set: any): [number, number] => [set[0], set[1]]) : [],
-                aces: el.aces !== undefined && Array.isArray(el.aces) ? [el.aces[0], el.aces[1]] : [0, 0],
-                smashedRackets: el.smashedRackets !== undefined && Array.isArray(el.smashedRackets) ? [el.smashedRackets[0], el.smashedRackets[1]] : [0, 0],
-                doubleFaults: el.doubleFaults !== undefined && Array.isArray(el.doubleFaults) ? [el.doubleFaults[0], el.doubleFaults[1]] : [0, 0]
-              };
-            });
-          }
           resolve(result);
         } else {
           reject(err);
         }
       });
     });
+  }
+
+  protected loadPlayers (parsedData: any): Player[] {
+    if (parsedData.players !== undefined && Array.isArray(parsedData.players)) {
+      return parsedData.players.map((el: any): Player => {
+        return {
+          id: el.id,
+          name: el.name
+        };
+      });
+    } else {
+      return [];
+    }
+  }
+
+  protected loadMatches (parsedData: any): Match[] {
+    if (parsedData.matches !== undefined && Array.isArray(parsedData.matches)) {
+      return parsedData.matches.map((el: any): Match => {
+        return {
+          playerId: el.playerId,
+          opponentId: el.opponentId,
+          result: el.result !== undefined && Array.isArray(el.result) ? el.result.map((set: any): [number, number] => [set[0], set[1]]) : [],
+          aces: el.aces !== undefined && Array.isArray(el.aces) ? [el.aces[0], el.aces[1]] : [0, 0],
+          smashedRackets: el.smashedRackets !== undefined && Array.isArray(el.smashedRackets) ? [el.smashedRackets[0], el.smashedRackets[1]] : [0, 0],
+          doubleFaults: el.doubleFaults !== undefined && Array.isArray(el.doubleFaults) ? [el.doubleFaults[0], el.doubleFaults[1]] : [0, 0]
+        };
+      });
+    } else {
+      return [];
+    }
   }
 }
