4b493b58704d151deab0240c6df5ba524cb2c892
Eduard Budacu <eduard.budacu@gmail.com>
2023-11-15 10:54:32 +0100

    used mock for readFile function

--- __tests__/unit/dataProvider.test.ts
+++ __tests__/unit/dataProvider.test.ts
@@ -1,10 +1,15 @@
 import DataProvider from '../../src/dataProvider';
 import data from '../../data/player_data.json';
+import fs from 'fs';
 
 describe('test data provider', () => {
   it('create object data provider', async () => {
+    const spy = jest.spyOn(fs, 'readFile')
+      .mockImplementation((_, callback) => { callback(null, Buffer.from(JSON.stringify(data))); });
+
     const obj = new DataProvider();
     const result = await obj.load();
+    expect(spy).toHaveBeenCalled();
     expect(result.players).toHaveLength(4);
     expect(result.matches).toHaveLength(6);
     expect(result).toMatchObject(data[0]);
